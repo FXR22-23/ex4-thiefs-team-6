fac23d89359c3986fe7c4a9772c4fb54413663b5
TheLion10 <itzhak.galperin@mail.huji.ac.il>
2023-05-18 21:34:14 +0300

    Added heartbeats(not good)

---
+++ Assets/Scripts/Heartbeats.cs
@@ -0,0 +1,26 @@
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heartbeats : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance instance;
+
+    public EventReference fmodEvent;
+
+    [SerializeField] [Range(0, 100)]
+    private float intensity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        instance.setParameterByName("Intensity", intensity);
+    }
+}
